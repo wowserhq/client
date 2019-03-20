31b92a136d0498ec9ba2f85a624a11d1ecaf4a25
Tim Kurvers <tim@moonsphere.net>
2019-03-20 22:55:16 +0100

    Add global Lua functions

---
+++ src/script/globals/extra.mjs
@@ -0,0 +1,120 @@
+export const setglobal = () => {
+  return 1;
+};
+
+export const getglobal = () => {
+  return 1;
+};
+
+export const strtrim = () => {
+  return 1;
+};
+
+export const strsplit = () => {
+  return 1;
+};
+
+export const strjoin = () => {
+  return 1;
+};
+
+export const strreplace = () => {
+  return 1;
+};
+
+export const strconcat = () => {
+  return 1;
+};
+
+export const strlenutf8 = () => {
+  return 1;
+};
+
+export const issecure = () => {
+  return 1;
+};
+
+export const issecurevariable = () => {
+  return 1;
+};
+
+export const forceinsecure = () => {
+  return 1;
+};
+
+export const securecall = () => {
+  return 1;
+};
+
+export const hooksecurefunc = () => {
+  return 1;
+};
+
+export const debugload = () => {
+  return 1;
+};
+
+export const debuginfo = () => {
+  return 1;
+};
+
+export const debugprint = () => {
+  return 1;
+};
+
+export const debugdump = () => {
+  return 1;
+};
+
+export const debugbreak = () => {
+  return 1;
+};
+
+export const debughook = () => {
+  return 1;
+};
+
+export const debugtimestamp = () => {
+  return 1;
+};
+
+export const debugprofilestart = () => {
+  return 1;
+};
+
+export const debugprofilestop = () => {
+  return 1;
+};
+
+export const seterrorhandler = () => {
+  // TODO: Implement Lua-side error handler
+  return 0;
+};
+
+export const geterrorhandler = () => {
+  return 1;
+};
+
+export const date = () => {
+  return 1;
+};
+
+export const time = () => {
+  return 1;
+};
+
+export const difftime = () => {
+  return 1;
+};
+
+export const debugstack = () => {
+  return 1;
+};
+
+export const debuglocals = () => {
+  return 1;
+};
+
+export const scrub = () => {
+  return 1;
+};
---
+++ src/script/globals/system.mjs
@@ -0,0 +1,23 @@
+export const GetTime = () => {
+  return 1;
+};
+
+export const GetGameTime = () => {
+  return 1;
+};
+
+export const ConsoleExec = () => {
+  return 1;
+};
+
+export const ReadFile = () => {
+  return 1;
+};
+
+export const DeleteFile = () => {
+  return 1;
+};
+
+export const AppendToFile = () => {
+  return 1;
+};
