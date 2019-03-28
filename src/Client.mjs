2a6952caf3c798387860e6d3226ce2da11be766a
Tim Kurvers <tim@moonsphere.net>
2019-03-28 20:54:38 +0100

    Expose fetch as method on client

--- src/Client.mjs
+++ src/Client.mjs
@@ -1,9 +1,12 @@
 import ScriptContext from './script/Context';
 import UIContext from './ui/Context';
+import { fetch } from './utils';
 
 class Client {
   constructor() {
     this.script = new ScriptContext(this);
+    this.fetch = fetch;
+
     this.ui = new UIContext(this);
   }
 }
