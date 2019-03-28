d353c45ce76febf249b1b4b66e0964a37062980e
Tim Kurvers <tim@moonsphere.net>
2019-03-28 21:41:55 +0100

    Test functionality by loading GlueXML.toc

--- src/index.mjs
+++ src/index.mjs
@@ -5,21 +5,8 @@ const client = new Client();
 
 console.log('Lift off!', client);
 
-const frame = new Frame(null);
-// const width = frame.m_width;
 
-const button = new Button();
-
-client.script.execute(
-  `
-  local frame1 = CreateFrame("FRAME", "TestFrame1", nil, nil, nil);
-  print(frame1)
-  -- local width = frame1:GetWidth();
-  -- print(width);
-
-  local frame2 = CreateFrame("FRAME", "TestFrame2", frame1, nil, nil);
-  print(frame2);
-  `,
-  'frame.lua',
-  0
-);
+(async () => {
+  await client.ui.load('Interface\\GlueXML\\GlueXML.toc');
+  console.log('Done!');
+})();
