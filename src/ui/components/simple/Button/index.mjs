78ca68da229775f2868bde17f4aab572370a60da
Tim Kurvers <tim@moonsphere.net>
2019-07-20 01:50:23 +0200

    Add Button script bindings

--- src/ui/components/simple/Button/index.mjs
+++ src/ui/components/simple/Button/index.mjs
@@ -1,6 +1,17 @@
 import Frame from '../Frame';
+import Script from '../../../scripting/Script';
 
 class Button extends Frame {
+  constructor(...args) {
+    super(...args);
+
+    this.scripts.register(
+      new Script('PreClick', ['button', 'down']),
+      new Script('OnClick', ['button', 'down']),
+      new Script('PostClick', ['button', 'down']),
+      new Script('OnDoubleClick', ['button']),
+    );
+  }
 }
 
 export default Button;
