b7487f9eb76a20f40d8990ebeca8f21087a87fc7
Tim Kurvers <tim@moonsphere.net>
2019-07-20 01:54:53 +0200

    Further flesh out loadXML for Frame

--- src/ui/components/simple/Frame/index.mjs
+++ src/ui/components/simple/Frame/index.mjs
@@ -1,6 +1,29 @@
+import DrawLayerType from '../../../../gfx/DrawLayerType';
+import FontString from '../FontString';
+import FrameStrataType from '../../abstract/FrameStrata/Type';
+import Region from '../Region';
+import Root from '../../Root';
 import Script from '../../../scripting/Script';
 import ScriptRegion from '../../abstract/ScriptRegion';
+import Texture from '../Texture';
+import UIContext from '../../../Context';
+import {
+  LinkedList,
+  LinkedListLink,
+  LinkedListNode,
+  stringToBoolean,
+} from '../../../../utils';
+
+import FrameFlag from './Flag';
 import * as scriptFunctions from './script';
+
+class FrameNode extends LinkedListNode {
+  constructor(frame) {
+    super();
+
+    this.frame = frame;
+  }
+}
 
 class Frame extends ScriptRegion {
   static get scriptFunctions() {
@@ -10,12 +33,27 @@ class Frame extends ScriptRegion {
   constructor(parent) {
     super();
 
+    this.flags = 0;
+
     this.loading = false;
     this.shown = false;
+    this.visible = false;
+    this.strataType = FrameStrataType.MEDIUM;
+    this.level = 0;
+
+    this.regions = LinkedList.of(Region, 'regionLink');
+    this.layers = LinkedList.of(Region, 'layerLink');
+    this.children = LinkedList.of(FrameNode);
+
+    this.framesLink = LinkedListLink.for(this);
+    this.destroyedLink = LinkedListLink.for(this);
+    this.strataLink = LinkedListLink.for(this);
+
+    const root = Root.instance;
+    root.register(this);
 
     this.parent = parent;
 
-    // TODO: Draw layers
     this.scripts.register(
       new Script('OnLoad'),
       new Script('OnSizeChanged', ['w', 'h']),
@@ -41,15 +79,92 @@ class Frame extends ScriptRegion {
     this.show();
   }
 
-  preLoadXML(node) {
-    const { id, name } = node.attributes;
+  setFrameFlag(flag, on) {
+    if (on) {
+      this.flags |= flag;
+    } else {
+      this.flags &= ~flag;
+    }
+  }
 
-    if (name) {
-      this.name = name;
+  setFrameLevel(level, shiftChildren) {
+    console.error('TODO: frame level', level, shiftChildren, 'for', this);
+  }
+
+  setFrameStrataType(strataType) {
+    if (this.strataType === strataType) {
+      return;
     }
 
+    const root = Root.instance;
+
+    if (this.visible) {
+      root.hideFrame(this, true);
+    }
+
+    this.strataType = strataType;
+
+    if (this.visible) {
+      root.showFrame(this, true);
+    }
+
+    for (const child of this.children) {
+      child.frame.setFrameStrataType(strataType);
+    }
+  }
+
+  // TODO: Seems to be necessary with split getter/setters + inheritance
+  get parent() {
+    return this._parent;
+  }
+
+  set parent(parent) {
+    if (this._parent === parent) {
+      return;
+    }
+
+    // TODO: Handle detachment of previous parent (if any)
+
+    if (this.visible) {
+      this.hideThis();
+    }
+
+    this._parent = parent;
+
+    if (parent) {
+      this.setFrameStrataType(parent.strataType);
+      this.setFrameLevel(parent.level + 1, true);
+
+      // TODO: Alpha and scrolling adjustments
+    } else {
+      this.setFrameStrataType(FrameStrataType.MEDIUM);
+      this.setFrameLevel(0, true);
+
+      // TODO: Alpha and scrolling adjustments
+    }
+
+    if (parent) {
+      // TODO: Parent attachment protection
+      const node = new FrameNode(this);
+      parent.children.linkToHead(node);
+    }
+
+    if (this.shown) {
+      if (!parent || parent.visible) {
+        this.showThis();
+      }
+    }
+  }
+
+  preLoadXML(node) {
+    super.preLoadXML(node);
+
+    const id = node.attributes.get('id');
     if (id) {
-      // TODO: Assign identifier for this frame
+      const idValue = parseInt(id, 10);
+      if (idValue >= 0) {
+        this.id = idValue;
+      }
     }
 
     this.loading = true;
@@ -57,14 +172,201 @@ class Frame extends ScriptRegion {
 
     this.deferredResize = true;
 
-    // TODO: Handle title region
-    // TODO: Handle regions
+    // TODO: Handle deferred resize for title region
+
+    for (const region of this.regions) {
+      region.deferredResize = true;
+    }
   }
 
   loadXML(node) {
+    const dontSavePosition = node.attributes.get('dontSavePosition');
+    const frameLevel = node.attributes.get('frameLevel');
+    const frameStrata = node.attributes.get('frameStrata');
+    const hidden = node.attributes.get('hidden');
+    const inherits = node.attributes.get('inherits');
+    const movable = node.attributes.get('movable');
+    const resizable = node.attributes.get('resizable');
+    const toplevel = node.attributes.get('toplevel');
+
+    if (inherits) {
+      const templates = UIContext.instance.templates.acquireByList(inherits);
+      for (const template of templates) {
+        if (template) {
+          if (template.locked) {
+            // TODO: Error handling
+          } else {
+            this.loadXML(template.node);
+            template.release();
+          }
+        } else {
+          // TODO: Error handling
+        }
+      }
+    }
+
+    super.loadXML(node);
+
+    if (hidden) {
+      if (stringToBoolean(hidden)) {
+        this.hide();
+      } else {
+        this.show();
+      }
+    }
+
+    if (toplevel) {
+      this.setFrameFlag(FrameFlag.TOPLEVEL, stringToBoolean(toplevel));
+    }
+
+    if (movable) {
+      this.setFrameFlag(FrameFlag.MOVABLE, stringToBoolean(toplevel));
+    }
+
+    if (dontSavePosition) {
+      this.setFrameFlag(FrameFlag.DONT_SAVE_POSITION, stringToBoolean(dontSavePosition));
+    }
+
+    if (resizable) {
+      this.setFrameFlag(FrameFlag.RESIZABLE, stringToBoolean(resizable));
+    }
+
+    if (frameStrata) {
+      const strataType = FrameStrataType[frameStrata];
+      if (strataType) {
+        this.setFrameStrataType(strataType);
+      } else {
+        // TODO: Error handling
+      }
+    }
+
+    if (frameLevel) {
+      const level = parseInt(frameLevel, 10);
+
+      if (level > 0) {
+        this.setFrameLevel(level, true);
+      } else {
+        // TODO: Error handling
+      }
+    }
+
+    // TODO: Alpha
+
+    // TODO: Enable mouse events
+
+    // TODO: Enable keyboard events
+
+    // TODO: Clamped to screen behaviour
+
+    // TODO: Protected
+
+    // TODO: Depth
+
+    for (const child of node.children) {
+      const iname = child.name.toLowerCase();
+      switch (iname) {
+        // TODO: TitleRegion, ResizeBounds, Backdrop & HitRectInsects
+        case 'layers':
+          this.loadXMLLayers(child);
+          break;
+        case 'attributes':
+          // TODO: Load attributes
+          break;
+        case 'scripts':
+          this.loadXMLScripts(child);
+          break;
+      }
+    }
   }
 
-  postLoadXML(node) {
+  loadXMLLayers(node) {
+    for (const layer of node.children) {
+      if (layer.name.toLowerCase() !== 'layer') {
+        // TODO: Error handling
+        continue;
+      }
+
+      const level = layer.attributes.get('level');
+      const drawLayerType = DrawLayerType[level] || DrawLayerType.ARTWORK;
+
+      for (const layerChild of layer.children) {
+        const iname = layerChild.name.toLowerCase();
+        switch (iname) {
+          case 'texture':
+            const texture = Texture.fromXMLNode(layerChild, this);
+            texture.setFrame(this, drawLayerType, texture.shown);
+            break;
+          case 'fontstring':
+            const fontstring = FontString.fromXMLNode(layerChild, this);
+            fontstring.setFrame(this, drawLayerType, fontstring.shown);
+            break;
+          default:
+            // TODO: Error handling
+        }
+      }
+    }
+  }
+
+  loadXMLScripts(node) {
+    for (const child of node.children) {
+      const script = this.scripts.get(child.name);
+      if (script) {
+        script.loadXML(child);
+
+        // TODO: Register for events
+      } else {
+        // TOOD: Error handling
+      }
+    }
+  }
+
+  postLoadXML(node, status) {
+    this.loading = false;
+
+    // TODO: More stuff
+
+    if (this.visible) {
+      this.deferredResize = false;
+
+      if (this.titleRegion) {
+        this.titleRegion.deferredResize = false;
+      }
+
+      for (const region of this.regions) {
+        region.deferredResize = false;
+      }
+    }
+
+    this.postLoadXMLFrames(node, status);
+
+    // TODO: Alpha animations
+
+    this.runOnLoadScript();
+
+    if (this.visible) {
+      this.runOnShowScript();
+    }
+  }
+
+  postLoadXMLFrames(node) {
+    const ui = UIContext.instance;
+
+    const inherits = node.attributes.get('inherits');
+    if (inherits) {
+      const templates = ui.templates.acquireByList(inherits);
+      for (const template of templates) {
+        if (template && !template.locked) {
+          this.postLoadXMLFrames(template.node);
+        }
+      }
+    }
+
+    const frames = node.getChildByName('Frames');
+    if (frames) {
+      for (const frame of frames.children) {
+        ui.createFrame(frame, this);
+      }
+    }
   }
 
   show() {
