7da6242798abcccef703936913fe6ef261c06551
Tim Kurvers <tim@moonsphere.net>
2019-07-20 01:42:43 +0200

    Add dummy Lua script functions for Frame

---
+++ src/ui/components/simple/Frame/script.mjs
@@ -0,0 +1,339 @@
+export const GetTitleRegion = (L) => {
+  return 0;
+};
+
+export const CreateTitleRegion = (L) => {
+  return 0;
+};
+
+export const CreateTexture = (L) => {
+  return 0;
+};
+
+export const CreateFontString = (L) => {
+  return 0;
+};
+
+export const GetBoundsRect = (L) => {
+  return 0;
+};
+
+export const GetNumRegions = (L) => {
+  return 0;
+};
+
+export const GetRegions = (L) => {
+  return 0;
+};
+
+export const GetNumChildren = (L) => {
+  return 0;
+};
+
+export const GetChildren = (L) => {
+  return 0;
+};
+
+export const GetFrameStrata = (L) => {
+  return 0;
+};
+
+export const SetFrameStrata = (L) => {
+  return 0;
+};
+
+export const GetFrameLevel = (L) => {
+  return 0;
+};
+
+export const SetFrameLevel = (L) => {
+  return 0;
+};
+
+export const HasScript = (L) => {
+  return 0;
+};
+
+export const GetScript = (L) => {
+  return 0;
+};
+
+export const SetScript = (L) => {
+  return 0;
+};
+
+export const HookScript = (L) => {
+  return 0;
+};
+
+export const RegisterEvent = (L) => {
+  return 0;
+};
+
+export const UnregisterEvent = (L) => {
+  return 0;
+};
+
+export const RegisterAllEvents = (L) => {
+  return 0;
+};
+
+export const UnregisterAllEvents = (L) => {
+  return 0;
+};
+
+export const IsEventRegistered = (L) => {
+  return 0;
+};
+
+export const AllowAttributeChanges = (L) => {
+  return 0;
+};
+
+export const CanChangeAttribute = (L) => {
+  return 0;
+};
+
+export const GetAttribute = (L) => {
+  return 0;
+};
+
+export const SetAttribute = (L) => {
+  return 0;
+};
+
+export const GetEffectiveScale = (L) => {
+  return 0;
+};
+
+export const GetScale = (L) => {
+  return 0;
+};
+
+export const SetScale = (L) => {
+  return 0;
+};
+
+export const GetEffectiveAlpha = (L) => {
+  return 0;
+};
+
+export const GetAlpha = (L) => {
+  return 0;
+};
+
+export const SetAlpha = (L) => {
+  return 0;
+};
+
+export const GetID = (L) => {
+  return 0;
+};
+
+export const SetID = (L) => {
+  return 0;
+};
+
+export const SetToplevel = (L) => {
+  return 0;
+};
+
+export const IsToplevel = (L) => {
+  return 0;
+};
+
+export const EnableDrawLayer = (L) => {
+  return 0;
+};
+
+export const DisableDrawLayer = (L) => {
+  return 0;
+};
+
+export const Show = (L) => {
+  return 0;
+};
+
+export const Hide = (L) => {
+  return 0;
+};
+
+export const IsVisible = (L) => {
+  return 0;
+};
+
+export const IsShown = (L) => {
+  return 0;
+};
+
+export const Raise = (L) => {
+  return 0;
+};
+
+export const Lower = (L) => {
+  return 0;
+};
+
+export const GetHitRectInsets = (L) => {
+  return 0;
+};
+
+export const SetHitRectInsets = (L) => {
+  return 0;
+};
+
+export const GetClampRectInsets = (L) => {
+  return 0;
+};
+
+export const SetClampRectInsets = (L) => {
+  return 0;
+};
+
+export const GetMinResize = (L) => {
+  return 0;
+};
+
+export const SetMinResize = (L) => {
+  return 0;
+};
+
+export const GetMaxResize = (L) => {
+  return 0;
+};
+
+export const SetMaxResize = (L) => {
+  return 0;
+};
+
+export const SetMovable = (L) => {
+  return 0;
+};
+
+export const IsMovable = (L) => {
+  return 0;
+};
+
+export const SetDontSavePosition = (L) => {
+  return 0;
+};
+
+export const GetDontSavePosition = (L) => {
+  return 0;
+};
+
+export const SetResizable = (L) => {
+  return 0;
+};
+
+export const IsResizable = (L) => {
+  return 0;
+};
+
+export const StartMoving = (L) => {
+  return 0;
+};
+
+export const StartSizing = (L) => {
+  return 0;
+};
+
+export const StopMovingOrSizing = (L) => {
+  return 0;
+};
+
+export const SetUserPlaced = (L) => {
+  return 0;
+};
+
+export const IsUserPlaced = (L) => {
+  return 0;
+};
+
+export const SetClampedToScreen = (L) => {
+  return 0;
+};
+
+export const IsClampedToScreen = (L) => {
+  return 0;
+};
+
+export const RegisterForDrag = (L) => {
+  return 0;
+};
+
+export const EnableKeyboard = (L) => {
+  return 0;
+};
+
+export const IsKeyboardEnabled = (L) => {
+  return 0;
+};
+
+export const EnableMouse = (L) => {
+  return 0;
+};
+
+export const IsMouseEnabled = (L) => {
+  return 0;
+};
+
+export const EnableMouseWheel = (L) => {
+  return 0;
+};
+
+export const IsMouseWheelEnabled = (L) => {
+  return 0;
+};
+
+export const EnableJoystick = (L) => {
+  return 0;
+};
+
+export const IsJoystickEnabled = (L) => {
+  return 0;
+};
+
+export const GetBackdrop = (L) => {
+  return 0;
+};
+
+export const SetBackdrop = (L) => {
+  return 0;
+};
+
+export const GetBackdropColor = (L) => {
+  return 0;
+};
+
+export const SetBackdropColor = (L) => {
+  return 0;
+};
+
+export const GetBackdropBorderColor = (L) => {
+  return 0;
+};
+
+export const SetBackdropBorderColor = (L) => {
+  return 0;
+};
+
+export const SetDepth = (L) => {
+  return 0;
+};
+
+export const GetDepth = (L) => {
+  return 0;
+};
+
+export const GetEffectiveDepth = (L) => {
+  return 0;
+};
+
+export const IgnoreDepth = (L) => {
+  return 0;
+};
+
+export const IsIgnoringDepth = (L) => {
+  return 0;
+};
