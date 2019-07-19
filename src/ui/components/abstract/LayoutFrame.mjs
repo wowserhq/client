8e057c651df6c11b6ebb12ab438fb6d41fb0c4f1
Tim Kurvers <tim@moonsphere.net>
2019-07-20 01:11:47 +0200

    Prevent layout flags from clashing with generic frame flags

--- src/ui/components/abstract/LayoutFrame.mjs
+++ src/ui/components/abstract/LayoutFrame.mjs
@@ -1,23 +1,33 @@
 class LayoutFrame {
-  constructor(ui) {
-    this.ui = ui;
+  constructor() {
+    this.layoutFlags = 0;
 
-    // TODO: Risk of flags prop clashing with other classes in hierarchy?
-    this.flags = 0;
+    this.rect = new Rect();
+    this._width = 0;
+    this._height = 0;
+
+    this.layoutScale = 1.0;
+    this.layoutDepth = 1.0;
+
+    this.guard = 0;
+  }
+
+  get layoutParent() {
+    return null;
   }
 
   set deferredResize(enable) {
     if (enable) {
-      this.flags |= 0x2;
+      this.layoutFlags |= 0x2;
 
       // TODO: Remove this layout frame from pending list if it's linked
 
       return;
     }
 
-    this.flags &= ~0x2;
+    this.layoutFlags &= ~0x2;
 
-    if (this.flags & 0x4) {
+    if (this.layoutFlags & 0x4) {
       this.resize(true);
     }
   }
