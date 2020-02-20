fdfeb59acdb09d06400c34bd0f6b3ab363efa5ba
Tim Kurvers <tim@moonsphere.net>
2020-02-20 16:31:22 +0100

    Add scripting interface for Texture

---
+++ src/ui/components/simple/Texture/script.mjs
@@ -0,0 +1,115 @@
+export const IsObjectType = () => {
+  return 0;
+};
+
+export const GetObjectType = () => {
+  return 0;
+};
+
+export const GetDrawLayer = () => {
+  return 0;
+};
+
+export const SetDrawLayer = () => {
+  return 0;
+};
+
+export const GetBlendMode = () => {
+  return 0;
+};
+
+export const SetBlendMode = () => {
+  return 0;
+};
+
+export const GetVertexColor = () => {
+  return 0;
+};
+
+export const SetVertexColor = () => {
+  return 0;
+};
+
+export const SetGradient = () => {
+  return 0;
+};
+
+export const SetGradientAlpha = () => {
+  return 0;
+};
+
+export const SetAlpha = () => {
+  return 0;
+};
+
+export const GetAlpha = () => {
+  return 0;
+};
+
+export const Show = () => {
+  return 0;
+};
+
+export const Hide = () => {
+  return 0;
+};
+
+export const IsVisible = () => {
+  return 0;
+};
+
+export const IsShown = () => {
+  return 0;
+};
+
+export const GetTexture = () => {
+  return 0;
+};
+
+export const SetTexture = () => {
+  return 0;
+};
+
+export const GetTexCoord = () => {
+  return 0;
+};
+
+export const SetTexCoord = () => {
+  return 0;
+};
+
+export const SetRotation = () => {
+  return 0;
+};
+
+export const SetDesaturated = () => {
+  return 0;
+};
+
+export const IsDesaturated = () => {
+  return 0;
+};
+
+export const SetNonBlocking = () => {
+  return 0;
+};
+
+export const GetNonBlocking = () => {
+  return 0;
+};
+
+export const SetHorizTile = () => {
+  return 0;
+};
+
+export const GetHorizTile = () => {
+  return 0;
+};
+
+export const SetVertTile = () => {
+  return 0;
+};
+
+export const GetVertTile = () => {
+  return 0;
+};
