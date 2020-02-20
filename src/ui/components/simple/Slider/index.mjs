6106a4ad0166b55521a2c0b9053c1ae7a63fa690
Tim Kurvers <tim@moonsphere.net>
2020-02-20 16:31:39 +0100

    Add scripting interface for Slider

--- src/ui/components/simple/Slider/index.mjs
+++ src/ui/components/simple/Slider/index.mjs
@@ -1,6 +1,20 @@
 import Frame from '../Frame';
 
+import * as scriptFunctions from './script';
+
 class Slider extends Frame {
+  static get scriptFunctions() {
+    return {
+      ...super.scriptFunctions,
+      ...scriptFunctions,
+    };
+  }
+
+  constructor(...args) {
+    super(...args);
+
+    this.value = 0.0;
+  }
 }
 
 export default Slider;
