ff5aaa63d868fdace4862c6ca31806fe9c49ca5a
Tim Kurvers <tim@moonsphere.net>
2020-02-20 16:11:01 +0100

    Add coordinate and dimension utilities

---
+++ src/utils/coordinates.mjs
@@ -0,0 +1,31 @@
+export let aspectRatio = null;
+export let aspectCompensation = null;
+export let maxAspectCompensation = null;
+let x = null;
+let y = null;
+
+export const setAspectRatio = (newAspectRatio) => {
+  aspectRatio = newAspectRatio;
+  aspectCompensation = aspectRatio * 0.75;
+  maxAspectCompensation = aspectCompensation * 1024.0;
+
+  y = 1.0 / Math.sqrt(aspectRatio * aspectRatio + 1.0);
+  x = aspectRatio * y;
+};
+
+export const DDCtoNDCWidth = ddcx => ddcx / x;
+export const DDCtoNDCHeight = ddcy => ddcy / y;
+export const DDCtoNDC = (ddcx, ddcy) => ({
+  x: DDCtoNDCWidth(ddcx),
+  y: DDCtoNDCHeight(ddcy),
+});
+
+export const NDCtoDDCWidth = ndcx => ndcx * x;
+export const NDCtoDDCHeight = ndcy => ndcy * y;
+export const NDCtoDDC = (ndcx, ndcy) => ({
+  x: NDCtoDDCWidth(ndcx),
+  y: NDCtoDDCHeight(ndcy),
+});
+
+// Set default aspect ratio
+setAspectRatio(1.3333333333333333);
