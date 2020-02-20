815c6b0796c192d6893f35ebf0a116f6a501a84d
Tim Kurvers <tim@moonsphere.net>
2020-02-20 16:05:36 +0100

    Add Lua utils

---
+++ src/utils/lua.mjs
@@ -0,0 +1,69 @@
+/* eslint-disable import/prefer-default-export */
+
+import {
+  LUA_TBOOLEAN,
+  LUA_TFUNCTION,
+  LUA_TLIGHTUSERDATA,
+  LUA_TNIL,
+  LUA_TNONE,
+  LUA_TNUMBER,
+  LUA_TSTRING,
+  LUA_TTABLE,
+  LUA_TUSERDATA,
+  lua_gettop,
+  lua_toboolean,
+  lua_tocfunction,
+  lua_tojsstring,
+  lua_tonumber,
+  lua_touserdata,
+  lua_type,
+} from '../ui/scripting/lua';
+
+const stackToArray = (L) => {
+  const top = lua_gettop(L);
+
+  const results = [];
+  for (let idx = 1; idx <= top; ++idx) {
+    const type = lua_type(L, idx);
+    let value;
+    switch (lua_type(L, idx)) {
+      case LUA_TBOOLEAN:
+        value = lua_toboolean(L, idx);
+        break;
+      case LUA_TFUNCTION:
+        value = lua_tocfunction(L, idx);
+        if (!value) {
+          value = '<lua closure>';
+        }
+        break;
+      case LUA_TLIGHTUSERDATA:
+        value = lua_touserdata(L, idx);
+        break;
+      case LUA_TNIL:
+        value = null;
+        break;
+      case LUA_TNONE:
+        value = undefined;
+      case LUA_TNUMBER:
+        value = lua_tonumber(L, idx);
+        break;
+      case LUA_TSTRING:
+        value = lua_tojsstring(L, idx);
+        break;
+      case LUA_TTABLE:
+        value = '<table>';
+        break;
+      case LUA_TUSERDATA:
+        value = '<userdata>';
+        break;
+      default:
+        throw new Error(`unknown type in stack: ${type}`);
+    }
+    results.push(value);
+  }
+  return results;
+};
+
+export {
+  stackToArray,
+};
