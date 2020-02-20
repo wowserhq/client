8acfff002ff407e551a6721c005e662ac511aa07
Tim Kurvers <tim@moonsphere.net>
2020-02-20 16:31:56 +0100

    Add scripting interface for Model

---
+++ src/ui/components/simple/Model/script.mjs
@@ -0,0 +1,95 @@
+export const SetModel = () => {
+  return 0;
+};
+
+export const GetModel = () => {
+  return 0;
+};
+
+export const ClearModel = () => {
+  return 0;
+};
+
+export const SetPosition = () => {
+  return 0;
+};
+
+export const SetFacing = () => {
+  return 0;
+};
+
+export const SetModelScale = () => {
+  return 0;
+};
+
+export const SetSequence = () => {
+  return 0;
+};
+
+export const SetSequenceTime = () => {
+  return 0;
+};
+
+export const SetCamera = () => {
+  return 0;
+};
+
+export const SetLight = () => {
+  return 0;
+};
+
+export const GetLight = () => {
+  return 0;
+};
+
+export const GetPosition = () => {
+  return 0;
+};
+
+export const GetFacing = () => {
+  return 0;
+};
+
+export const GetModelScale = () => {
+  return 0;
+};
+
+export const AdvanceTime = () => {
+  return 0;
+};
+
+export const ReplaceIconTexture = () => {
+  return 0;
+};
+
+export const SetFogColor = () => {
+  return 0;
+};
+
+export const GetFogColor = () => {
+  return 0;
+};
+
+export const SetFogNear = () => {
+  return 0;
+};
+
+export const GetFogNear = () => {
+  return 0;
+};
+
+export const SetFogFar = () => {
+  return 0;
+};
+
+export const GetFogFar = () => {
+  return 0;
+};
+
+export const ClearFog = () => {
+  return 0;
+};
+
+export const SetGlow = () => {
+  return 0;
+};
