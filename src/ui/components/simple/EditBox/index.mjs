1f1c1ba2bd5a69ecd2ad01c62cf62f03db259cc3
Tim Kurvers <tim@moonsphere.net>
2020-02-20 17:26:54 +0100

    Add event hooks for EditBox

--- src/ui/components/simple/EditBox/index.mjs
+++ src/ui/components/simple/EditBox/index.mjs
@@ -1,6 +1,24 @@
 import Frame from '../Frame';
+import Script from '../../../scripting/Script';
 
 class EditBox extends Frame {
+  constructor(...args) {
+    super(...args);
+
+    this.scripts.register(
+      new Script('OnEnterPressed'),
+      new Script('OnEscapePressed'),
+      new Script('OnSpacePressed'),
+      new Script('OnTabPressed'),
+      new Script('OnTextChanged', ['userInput']),
+      new Script('OnTextSet'),
+      new Script('OnCursorChanged', ['x', 'y', 'w', 'h']),
+      new Script('OnInputLanguageChanged', ['language']),
+      new Script('OnEditFocusGained'),
+      new Script('OnEditFocusLost'),
+      new Script('OnCharComposition', ['text']),
+    );
+  }
 }
 
 export default EditBox;
