9f89c68198b3aa2636828d13790fd2e535ff69a3
Tim Kurvers <tim@moonsphere.net>
2020-02-26 01:55:10 +0100

    Add scripting interface for HTML

---
+++ src/ui/components/simple/HTML/script.mjs
@@ -0,0 +1,91 @@
+export const SetFontObject = () => {
+  return 0;
+};
+
+export const GetFontObject = () => {
+  return 0;
+};
+
+export const SetFont = () => {
+  return 0;
+};
+
+export const GetFont = () => {
+  return 0;
+};
+
+export const SetTextColor = () => {
+  return 0;
+};
+
+export const GetTextColor = () => {
+  return 0;
+};
+
+export const SetShadowColor = () => {
+  return 0;
+};
+
+export const GetShadowColor = () => {
+  return 0;
+};
+
+export const SetShadowOffset = () => {
+  return 0;
+};
+
+export const GetShadowOffset = () => {
+  return 0;
+};
+
+export const SetSpacing = () => {
+  return 0;
+};
+
+export const GetSpacing = () => {
+  return 0;
+};
+
+export const SetJustifyH = () => {
+  return 0;
+};
+
+export const GetJustifyH = () => {
+  return 0;
+};
+
+export const SetJustifyV = () => {
+  return 0;
+};
+
+export const GetJustifyV = () => {
+  return 0;
+};
+
+export const SetIndentedWordWrap = () => {
+  return 0;
+};
+
+export const GetIndentedWordWrap = () => {
+  return 0;
+};
+
+export const SetText = () => {
+  return 0;
+};
+
+export const SetHyperlinkFormat = () => {
+  return 0;
+};
+
+export const GetHyperlinkFormat = () => {
+  return 0;
+};
+
+export const SetHyperlinksEnabled = () => {
+  return 0;
+};
+
+export const GetHyperlinksEnabled = () => {
+  return 0;
+};
