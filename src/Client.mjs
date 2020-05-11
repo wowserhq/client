44ba98d4039f1a17df00ebed974a2e20aea33f63
Tim Kurvers <tim@moonsphere.net>
2020-05-11 15:02:14 +0200

    Render screen using WebGL2 device with debug lines

--- src/Client.mjs
+++ src/Client.mjs
@@ -1,14 +1,28 @@
+import WebGL2Device from './gfx/apis/webgl2/WebGL2Device';
+import WebGPUDevice from './gfx/apis/webgpu/WebGPUDevice';
 import Screen from './gfx/Screen';
+import TextureRegistry from './gfx/Texture/Registry';
 import UIContext from './ui/Context';
 import { fetch } from './utils';
 
 class Client {
-  constructor() {
+  constructor(canvas, { api }) {
     this.constructor.instance = this;
 
     this.fetch = fetch;
 
-    this.screen = new Screen();
+    switch (api) {
+      default:
+      case 'webgl2':
+        this.device = new WebGL2Device(canvas);
+        break;
+      case 'webgpu':
+        this.device = new WebGPUDevice(canvas);
+        break;
+    }
+
+    this.screen = new Screen(canvas);
+    this.textures = new TextureRegistry();
     this.ui = new UIContext();
   }
 }
--- src/gfx/Screen/index.mjs
+++ src/gfx/Screen/index.mjs
@@ -1,18 +1,21 @@
+import Device from '../Device';
 import { LinkedList, LinkStrategy } from '../../utils';
 
 import ScreenLayer from './Layer';
 
 class Screen {
-  constructor() {
+  constructor(canvas) {
     this.constructor.instance = this;
 
-    this.element = null;
+    this.canvas = canvas;
     this.layers = LinkedList.of(ScreenLayer, 'zorderLink');
 
     this.render = this.render.bind(this);
 
     // TODO: Continuously render screen, not on click
     document.addEventListener('click', this.render);
+
+    this.debugLines();
   }
 
   getViewport() {
@@ -23,23 +26,23 @@ class Screen {
     // TODO: Set viewport
   }
 
-  attach(element) {
-    this.element = element;
-
-    // TODO: Initial render
-    // requestAnimationFrame(this.render);
-  }
-
-  detach() {
-    this.element = null;
-  }
-
   render() {
-    if (!this.element) {
-      return;
-    }
+    const { gl } = Device.instance;
 
     console.group('render');
+
+    // Ensure canvas is properly sized
+    const {
+      clientWidth: displayWidth,
+      clientHeight: displayHeight,
+      width,
+      height,
+    } = this.canvas;
+
+    if (width !== displayWidth || height !== displayHeight) {
+      this.canvas.width = displayWidth;
+      this.canvas.height = displayHeight;
+    }
 
     // TODO: Base rect
 
@@ -54,6 +57,11 @@ class Screen {
     }
 
     const _viewport = this.viewport;
+    // TODO: Save viewport
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
     for (const layer of this.layers) {
       const { visibleRect } = layer;
@@ -79,6 +87,15 @@ class Screen {
       layer.render(layer.param, layer.rect, layer.visibleRect, elapsedSecs);
     }
 
+    // TODO: Restore viewport
+
+    // TODO: Screen capturing
+
+    // const device = Device.instance;
+    // device.presentScene();
+
+    this.debugLines();
+
     console.groupEnd('render');
 
     // TODO: Render again
@@ -100,6 +117,64 @@ class Screen {
     }
 
     this.layers.link(layer, LinkStrategy.BEFORE, target);
+  }
+
+  debugLines() {
+    const device = Device.instance;
+    const { gl } = device;
+
+    if (!this.debugProgram) {
+      const pixelShader = device.shaders.pixelShaderFor('DebugLine');
+      const vertexShader = device.shaders.vertexShaderFor('DebugLine');
+      if (!pixelShader.isValid || !vertexShader.isValid) {
+        return;
+      }
+
+      this.debugProgram = gl.createProgram();
+      gl.attachShader(this.debugProgram, vertexShader.apiShader);
+      gl.attachShader(this.debugProgram, pixelShader.apiShader);
+      gl.linkProgram(this.debugProgram);
+
+      const success = gl.getProgramParameter(this.debugProgram, gl.LINK_STATUS);
+      if (!success) {
+        console.error(gl.getProgramInfoLog(this.debugProgram));
+        gl.deleteProgram(this.debugProgram);
+        return;
+      }
+    }
+
+    gl.useProgram(this.debugProgram);
+
+    const positionPtr = gl.getAttribLocation(this.debugProgram, 'position');
+
+    const vertical = (x) => [x, -1, x, 1];
+    const horizontal = (y) => [-1, y, 1, y];
+
+    const dataBuffer = gl.createBuffer();
+    const data = new Float32Array([
+      ...vertical(-1),
+      ...vertical(-0.5),
+      ...vertical(0),
+      ...vertical(0.5),
+      ...vertical(0.999),
+
+      ...horizontal(-1),
+      ...horizontal(-0.5),
+      ...horizontal(0),
+      ...horizontal(0.5),
+      ...horizontal(0.999),
+    ]);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, dataBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+    const vao = gl.createVertexArray();
+    gl.bindVertexArray(vao);
+
+    gl.enableVertexAttribArray(positionPtr);
+    gl.vertexAttribPointer(positionPtr, 2, gl.FLOAT, false, 0, 0);
+
+    gl.drawArrays(gl.LINES, 0, data.length / 2);
   }
 }
 
