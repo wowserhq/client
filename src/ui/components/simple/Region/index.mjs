f7864245e28279c099be50fe4b947ecf72672905
Tim Kurvers <tim@moonsphere.net>
2020-05-11 15:13:55 +0200

    Implement Region frame and show/hide behaviour

--- src/ui/components/simple/Region/index.mjs
+++ src/ui/components/simple/Region/index.mjs
@@ -1,21 +1,103 @@
 import ScriptRegion from '../../abstract/ScriptRegion';
+import { LinkedListLink } from '../../../../utils';
 
 class Region extends ScriptRegion {
   constructor(frame, drawLayerType, show) {
     super();
 
+    this.drawLayerType = null;
     this.shown = false;
     this.visible = false;
+
+    this.colors = [];
+    this.alphas = [];
+
+    this.layerLink = LinkedListLink.for(this);
+    this.regionLink = LinkedListLink.for(this);
 
     if (frame) {
       this.setFrame(frame, drawLayerType, show);
     }
   }
 
-  setFrame(frame, _drawLayerType, _show) {
-    this._parent = frame;
-
+  onRegionChanged() {
     // TODO: Implementation
+  }
+
+  setFrame(frame, drawLayerType, show) {
+    if (this._parent === frame) {
+      if (this.drawLayerType === drawLayerType) {
+        if (show !== this.shown) {
+          if (show) {
+            this.show();
+          } else {
+            this.hide();
+          }
+        }
+      } else {
+        if (this.shown) {
+          this.hide();
+        }
+
+        this.drawLayerType = drawLayerType;
+
+        if (show) {
+          this.show();
+        }
+      }
+    } else {
+      if (this._parent) {
+        this.hideThis();
+        this._parent.regions.unlink(this);
+      }
+
+      this._parent = frame;
+      this.drawLayerType = drawLayerType;
+
+      if (frame) {
+        frame.regions.add(this);
+        this.deferredResize = (this._parent.layoutFlags & 0x2);
+
+        // TODO: Color changed
+
+        if (show) {
+          this.show();
+        } else {
+          this.hide();
+        }
+      }
+    }
+  }
+
+  hide() {
+    this.shown = false;
+    this.hideThis();
+  }
+
+  hideThis() {
+    if (this.visible && this._parent) {
+      if (!this._parent.loading) {
+        this.deferredResize = true;
+      }
+      this._parent.removeRegion(this, this.drawLayerType);
+      this.visible = false;
+    }
+  }
+
+  show() {
+    this.shown = true;
+    this.showThis();
+  }
+
+  showThis() {
+    if (this.shown && this._parent && this._parent.visible && !this.visible) {
+      if (!this._parent.loading) {
+        this.deferredResize = false;
+      }
+
+      this._parent.addRegion(this, this.drawLayerType);
+      this.visible = 1;
+    }
   }
 }
 
