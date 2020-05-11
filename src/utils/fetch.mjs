cd63c215557f75d631500450ea229e8363e9ec27
Tim Kurvers <tim@moonsphere.net>
2020-05-11 14:31:57 +0200

    Support option for fetch utility to retrieve non-text response bodies

--- src/utils/fetch.mjs
+++ src/utils/fetch.mjs
@@ -1,8 +1,8 @@
-export default async (path) => {
+export default async (path, bodyType = 'text') => {
   // TODO: Does this path need to be normalized?
   const response = await fetch(path);
   if (response.status === 200) {
-    return response.text();
+    return response[bodyType]();
   } else {
     throw new Error(`Could not load ${path}: ${response.statusText}`);
   }
